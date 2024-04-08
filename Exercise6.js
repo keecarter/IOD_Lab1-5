2c4354443f717c224b510110dd2cba4bab3a5f77
keecarter <153045725+keecarter@users.noreply.github.com>
2024-04-08 15:47:47 -0400

    Add files via upload

---
+++ Exercise6.js
@@ -0,0 +1,16 @@
+const book = {
+    "title" : "Twilight" ,
+    "description" : "A teenage love drama about a girl, a vampire, and a werewolf." ,
+    "author" : "Stephenie Meyer" ,
+    "pages": 498
+}
+console.log(book);
+
+console.log(book.title);
+console.log(book.description);
+console.log(book.author);
+console.log(book.pages);
+
+book.description = "A love story about a teenage girl's unique experience of falling in love with a vampire.";
+console.log(book.description);
+
